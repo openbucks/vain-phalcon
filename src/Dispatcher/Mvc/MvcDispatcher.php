700453b47c2f886e6f09c20f10cb589455c4b8e5
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-07-15 13:52:51 +0300

    Refactored router decorator

---
+++ src/Dispatcher/Mvc/MvcDispatcher.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+namespace Vain\Phalcon\Dispatcher\Mvc;
+
+use Phalcon\Mvc\Dispatcher as PhalconMvcDispatcher;
+
+/**
+ * Class MvcDispatcher
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class MvcDispatcher extends PhalconMvcDispatcher
+{
+
+    private $separator;
+
+    /**
+     * MvcDispatcher constructor.
+     *
+     * @param $separator
+     */
+    public function __construct($separator)
+    {
+        $this->separator = $separator;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHandlerClass()
+    {
+        $this->_resolveEmptyProperties();
+
+		if (false === strpos("\\", $this->_handlerName, "\\")) {
+            $camelizedClass = Zephir\Utils::camelize($this->_handlerName);
+		} else {
+            $camelizedClass = $this->_handlerName;
+		}
+
+		if (null === $this->_namespaceName) {
+		    return $camelizedClass . $this->_handlerSuffix;
+        }
+
+        $namespace = $this->_namespaceName;
+        if ($this->separator !== substr($namespace, -1)) {
+            $namespace .= $this->separator;
+        }
+
+        return $namespace . $camelizedClass . $this->_handlerSuffix;
+    }
+}
