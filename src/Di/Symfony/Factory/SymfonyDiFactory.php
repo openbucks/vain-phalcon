27676f37a76a406c233dd9a18444811d00f7b843
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-07-22 10:14:20 +0300

    env support for di factory

--- src/Di/Symfony/Factory/SymfonyDiFactory.php
+++ src/Di/Symfony/Factory/SymfonyDiFactory.php
@@ -28,55 +28,73 @@ class SymfonyDiFactory implements DiFactoryInterface
 
     private $applicationPath;
 
-    private $configFile;
+    private $configDir;
 
-    private $cacheFile;
+    private $cacheDir;
 
     /**
      * SymfonyDiFactory constructor.
      * @param string $applicationDir
-     * @param string $configFile
-     * @param string $cacheFile
+     * @param string $configDir
+     * @param string $cacheDir
      */
-    public function __construct($applicationDir, $configFile, $cacheFile)
+    public function __construct($applicationDir, $configDir, $cacheDir)
     {
         $this->applicationPath = $applicationDir;
-        $this->configFile = $configFile;
-        $this->cacheFile = $cacheFile;
+        $this->configDir = $configDir;
+        $this->cacheDir = $cacheDir;
     }
 
     /**
      * @return SymfonyContainerBuilder
      */
-    protected function createContainer()
+    protected function createContainer($applicationPath, $configDir, $applicationEnv, $cachingEnabled)
     {
         $builder = new SymfonyContainerBuilder;
-        $loader = new YamlFileLoader($builder, new FileLocator($this->applicationPath));
-        $loader->load($this->configFile);
-        $builder->setParameter('app.dir', $this->applicationPath);
+        $loader = new YamlFileLoader($builder, new FileLocator($applicationPath));
+        $diConfig = sprintf('%s/%s/%s/di.yml', $applicationPath, $configDir, $applicationEnv);
+        $loader->load($diConfig);
+        $builder->setParameter('app.dir', $applicationPath);
+        $builder->setParameter('app.env', $applicationEnv);
+        $builder->setParameter('app.config.dir', $configDir);
+        $builder->setParameter('app.cache.dir', $this->cacheDir);
+        $builder->setParameter('app.caching', $cachingEnabled);
+
         $builder->compile();
 
         return $builder;
     }
 
     /**
+     * @param string $applicationPath
+     * @param string $cacheDir
+     * @param string $applicationEnv
+     *
+     * @return string
+     */
+    protected function getCachedContainerPath($applicationPath, $cacheDir, $applicationEnv)
+    {
+        return sprintf('%s/%s/container/di_%s.php', $applicationPath, $cacheDir, $applicationEnv);
+    }
+
+    /**
      * @param SymfonyContainerBuilder $container
-     * @param string $file
-     * 
+     * @param string $containerPath
+     *
      * @return SymfonyContainerBuilder
-     * 
+     *
      * @throws UnableToCacheContainerException
      */
-    protected function dumpContainer(SymfonyContainerBuilder $container, $file)
+    protected function dumpContainer(SymfonyContainerBuilder $container, $containerPath)
     {
         $dumper = new PhpDumper($container);
 
-        if (false === file_exists(dirname($file))) {
-            mkdir(dirname($file), 0755, true);
+        if (false === file_exists(dirname($containerPath))) {
+            mkdir(dirname($containerPath), 0755, true);
         }
 
-        if (false === file_put_contents($file, $dumper->dump(['class' => 'CachedSymfonyContainer']))) {
-            throw new UnableToCacheContainerException($this, $file);
+        if (false === file_put_contents($containerPath, $dumper->dump(['class' => 'CachedSymfonyContainer']))) {
+            throw new UnableToCacheContainerException($this, $containerPath);
         }
 
         return $container;
@@ -87,16 +105,23 @@ class SymfonyDiFactory implements DiFactoryInterface
      */
     public function createDi()
     {
-        $cachedContainerSource = $this->applicationPath . DIRECTORY_SEPARATOR . $this->cacheFile ;
-        if (false === file_exists($cachedContainerSource)) {
-            $container = $this->dumpContainer($this->createContainer(), $cachedContainerSource);
+        $applicationEnv = defined('APPLICATION_ENV') ? APPLICATION_ENV : 'dev';
+        $cachingEnabled = defined('APPLICATION_CACHE') ? (bool)APPLICATION_CACHE : false;
 
-            $container = new SymfonyContainerAdapter($container);
+        if (false === $cachingEnabled) {
+            $container = $this->createContainer($this->applicationPath, $this->configDir, $applicationEnv, $cachingEnabled);
 
-            return $container;
+            return new SymfonyContainerAdapter($container);
         }
 
-        require_once $cachedContainerSource;
+        $containerPath = $this->getCachedContainerPath($this->applicationPath, $this->cacheDir, $applicationEnv);
+        if (false === file_exists($containerPath)) {
+            $container = $this->dumpContainer($this->createContainer($this->applicationPath, $this->configDir, $applicationEnv, $cachingEnabled), $containerPath);
+
+            return new SymfonyContainerAdapter($container);
+        }
+
+        require_once $containerPath;
 
         return new SymfonyContainerAdapter(new \CachedSymfonyContainer());
     }
