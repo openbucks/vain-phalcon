9c7dd712de58b9a6f88d6c9775bca10d9db592f7
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-02 09:36:23 +0300

    Added exceptions to ApiConfigProvider

---
+++ src/Exception/NoModuleConfigDataException.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+namespace Vain\Phalcon\Exception;
+
+use Vain\Api\Config\Provider\ApiConfigProviderInterface;
+use Vain\Api\Exception\ConfigProviderException;
+use Vain\Http\Request\VainServerRequestInterface;
+
+/**
+ * Class NoModuleConfigDataException
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class NoModuleConfigDataException extends ConfigProviderException
+{
+    private $request;
+
+    /**
+     * NoConfigDataException constructor.
+     *
+     * @param ApiConfigProviderInterface $apiConfigProvider
+     * @param VainServerRequestInterface $request
+     * @param string                     $moduleName
+     */
+    public function __construct(
+        ApiConfigProviderInterface $apiConfigProvider,
+        VainServerRequestInterface $request,
+        $moduleName
+    ) {
+        $this->request = $request;
+        parent::__construct($apiConfigProvider, sprintf('Cannot find api config for module %s', $moduleName), 0, null);
+    }
+
+    /**
+     * @return VainServerRequestInterface
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+}
