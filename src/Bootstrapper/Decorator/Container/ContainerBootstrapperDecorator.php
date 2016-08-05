cf6fa151fd331715d178dc2690c76ab29a080a00
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 10:01:30 +0300

    Added extension support

---
+++ src/Bootstrapper/Decorator/Container/ContainerBootstrapperDecorator.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+namespace Vain\Phalcon\Bootstrapper\Decorator\Container;
+
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Vain\Phalcon\Bootstrapper\Decorator\AbstractBootstrapperDecorator;
+use Phalcon\Application as PhalconApplication;
+use Vain\Phalcon\Di\Compile\CompileAwareContainerInterface;
+use Vain\Phalcon\Exception\UnableToCacheContainerException;
+
+/**
+ * Class ContainerBootstrapperDecorator
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ContainerBootstrapperDecorator extends AbstractBootstrapperDecorator
+{
+    /**
+     * @param CompileAwareContainerInterface $container
+     * @param string                         $containerPath
+     *
+     * @return CompileAwareContainerInterface
+     *
+     * @throws UnableToCacheContainerException
+     */
+    protected function dumpContainer(CompileAwareContainerInterface $container, $containerPath)
+    {
+        $dumper = new PhpDumper($container);
+        if (false === file_exists(dirname($containerPath))) {
+            mkdir(dirname($containerPath), 0755, true);
+        }
+        if (false === file_put_contents($containerPath, $dumper->dump(['class' => 'CachedSymfonyContainer']))) {
+            throw new UnableToCacheContainerException($this, $containerPath);
+        }
+
+        return $container;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function bootstrap(PhalconApplication $application)
+    {
+        /**
+         * @var CompileAwareContainerInterface $diContainer
+         */
+        $diContainer = $application->getDI();
+        if ($diContainer->has('app.caching')
+            && $diContainer->has('app.container.path')
+            && $diContainer->get('app.caching')
+            && false === file_exists($containerPath = $diContainer->get('app.container.path'))
+        ) {
+            $this->dumpContainer($diContainer, $containerPath);
+        }
+
+        return parent::bootstrap($application);
+    }
+}
