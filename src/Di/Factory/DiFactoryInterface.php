1d18b46b1c11355fe671f910a58abdbdf0ed2d3f
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 09:33:11 +0300

    Changed diFactory signature

--- src/Di/Factory/DiFactoryInterface.php
+++ src/Di/Factory/DiFactoryInterface.php
@@ -5,15 +5,22 @@
  * Date: 5/20/16
  * Time: 11:48 AM
  */
-
 namespace Vain\Phalcon\Di\Factory;
 
 use \Phalcon\DiInterface as PhalconDiInterface;
 
+/**
+ * Interface DiFactoryInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
 interface DiFactoryInterface
 {
     /**
+     * @param string $applicationEnv
+     * @param bool   $cachingEnabled
+     *
      * @return PhalconDiInterface
      */
-    public function createDi();
+    public function createDi($applicationEnv, $cachingEnabled);
 }
