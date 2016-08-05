1b1f1d89fd3ad5e657481391d82a4f23aaed37ac
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 11:21:22 +0300

    Fixed bootstrappers

---
+++ src/Di/Compile/CompileDiFactory.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+namespace Vain\Phalcon\Di\Compile;
+
+use Vain\Phalcon\Di\Factory\Decorator\Compile\CompileDiFactoryDecorator;
+use Vain\Phalcon\Di\Factory\Decorator\Extension\ExtensionDiFactoryDecorator;
+use Vain\Phalcon\Di\Factory\DiFactoryInterface;
+use Vain\Phalcon\Di\Symfony\Factory\SymfonyDiFactory;
+
+/**
+ * Class CompileDiFactory
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class CompileDiFactory implements DiFactoryInterface
+{
+    private $applicationDir;
+
+    private $configDir;
+
+    private $cacheDir;
+
+    /**
+     * CompileDiFactory constructor.
+     *
+     * @param $applicationDir
+     * @param $configDir
+     * @param $cacheDir
+     */
+    public function __construct($applicationDir, $configDir, $cacheDir)
+    {
+        $this->applicationDir = $applicationDir;
+        $this->configDir = $configDir;
+        $this->cacheDir = $cacheDir;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createDi($applicationEnv, $cachingEnabled)
+    {
+        return new CompileDiFactoryDecorator(
+            new ExtensionDiFactoryDecorator(
+                new SymfonyDiFactory($this->applicationDir, $this->configDir, $this->cacheDir)
+            )
+        );
+    }
+}
--- src/Di/Factory/Decorator/Extension/ExtensionDiFactoryDecorator.php
+++ src/Di/Factory/Decorator/Extension/ExtensionDiFactoryDecorator.php
@@ -8,30 +8,28 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://github.com/allflame/vain-phalcon
  */
-namespace Vain\Phalcon\Bootstrapper\Decorator\Extension;
+namespace Vain\Phalcon\Di\Factory\Decorator\Extension;
 
-use Vain\Phalcon\Bootstrapper\Decorator\AbstractBootstrapperDecorator;
 use Vain\Phalcon\Di\Compile\CompileAwareContainerInterface;
-use Phalcon\Application as PhalconApplication;
+use Vain\Phalcon\Di\Factory\Decorator\AbstractDiFactoryDecorator;
 
 /**
- * Class ExtensionBootstrapperDecorator
+ * Class ExtensionDiFactoryDecorator
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-class ExtensionBootstrapperDecorator extends AbstractBootstrapperDecorator
+class ExtensionDiFactoryDecorator extends AbstractDiFactoryDecorator
 {
+
     /**
      * @inheritDoc
      */
-    public function bootstrap(PhalconApplication $application)
+    public function createDi($applicationEnv, $cachingEnabled)
     {
-        parent::bootstrap($application);
-
         /**
          * @var CompileAwareContainerInterface $diContainer
          */
-        $diContainer = $application->getDI();
+        $diContainer = parent::createDi($applicationEnv, $cachingEnabled);
         if (false === $diContainer->isFrozen() && $diContainer->has('app.extensions')) {
             foreach ($diContainer->get('app.extensions') as $extension) {
                 if (false === $diContainer->has($extension)) {
@@ -41,6 +39,7 @@ class ExtensionBootstrapperDecorator extends AbstractBootstrapperDecorator
                 $diContainer->get($extension)->register($diContainer);
             }
         }
-        $diContainer->compile();
+
+        return $diContainer;
     }
 }
