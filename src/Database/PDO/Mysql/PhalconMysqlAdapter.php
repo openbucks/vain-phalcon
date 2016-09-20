daee8ecdb08a32cdbd56c1cc12210bba74ac476e
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-09-20 13:06:38 +0300

    Added Phalcon database adapters

---
+++ src/Database/PDO/Mysql/PhalconMysqlAdapter.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+
+namespace Vain\Phalcon\Database\PDO\Mysql;
+
+use Vain\Phalcon\Database\PDO\PhalconPDODatabaseAdapter;
+
+/**
+ * Class PhalconMysqlAdapter
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class PhalconMysqlAdapter extends PhalconPDODatabaseAdapter
+{
+}
