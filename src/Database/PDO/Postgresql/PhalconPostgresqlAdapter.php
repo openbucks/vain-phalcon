12f0ddd4e765a9954f4cbe1d2c4a5c8247b38413
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-09-20 13:06:38 +0300

    Added Phalcon database adapters

---
+++ src/Database/PDO/Postgresql/PhalconPostgresqlAdapter.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+
+namespace Vain\Phalcon\Database\PDO\Postgresql;
+
+use Vain\Phalcon\Database\PDO\PhalconPDODatabaseAdapter;
+
+/**
+ * Class PhalconPostgresqlAdapter
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class PhalconPostgresqlAdapter extends PhalconPDODatabaseAdapter
+{
+}
