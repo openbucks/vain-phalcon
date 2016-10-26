755f10bb04d3a27fdc2e5d0b16d7d64874a822f7
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-10-26 13:43:31 +0300

    Added PhalconHttpContext, di config

---
+++ src/Http/Context/PhalconHttpContext.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-phalcon
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-phalcon
+ */
+
+namespace Vain\Phalcon\Http\Context;
+
+use Vain\Http\Context\AbstractContext;
+
+/**
+ * Class PhalconHttpContext
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class PhalconHttpContext extends AbstractContext
+{
+}
