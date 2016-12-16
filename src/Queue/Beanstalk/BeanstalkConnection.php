98f61aebc59bbc078ec84be4de48b339a7a967f8
allflame <taras.p.gyrnik@gmail.com>
2016-12-17 01:02:39 +0200

    Removed dependency

--- src/Queue/Beanstalk/BeanstalkConnection.php
+++ src/Queue/Beanstalk/BeanstalkConnection.php
@@ -13,41 +13,20 @@ declare(strict_types = 1);
 namespace Vain\Phalcon\Queue\Beanstalk;
 
 use Phalcon\Queue\Beanstalk;
-use Vain\Core\Connection\ConnectionInterface;
+use Vain\Core\Connection\AbstractConnection;
 
 /**
  * Class BeanstalkConnection
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-class BeanstalkConnection implements ConnectionInterface
+class BeanstalkConnection extends AbstractConnection
 {
-
-    private $configData;
-
-    /**
-     * BeanstalkConnection constructor.
-     *
-     * @param array $configData
-     */
-    public function __construct(array $configData)
-    {
-        $this->configData = $configData;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getName() : string
-    {
-        return $this->configData['type'];
-    }
-
     /**
      * @inheritDoc
      */
     public function establish()
     {
-        return new Beanstalk($this->configData);
+        return new Beanstalk($this->getConfigData());
     }
 }
